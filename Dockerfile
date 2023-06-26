a06987c6394da08cb75563c3a384f1b757291cb8
Alexander Mohan Morzeria-Davis <me+git@alexandermohan.com>
2023-06-26 18:22:18 +0100

    skip recaptcha initialisation during docker build
    Signed-off-by: Alexander Mohan Morzeria-Davis <me+git@alexandermohan.com>

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,10 @@ ADD . /app
 RUN echo "MAILERSEND_API_TOKEN=null" > .env
 RUN bundle
 RUN npm i -g yarn && yarn install
+ENV ASSET_PRECOMPILE=1
 RUN rake assets:precompile
 RUN rm -rf .env
+ENV ASSET_PRECOMPILE=
 EXPOSE 3000
 ENTRYPOINT ["bin/prod"]
 LABEL org.opencontainers.image.description="Web app for Bronwyn Sweeney with editable content blocks and smart features"
--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,12 +1,16 @@
-Recaptcha.configure do |config|
-    config.site_key  = Rails.application.credentials.recaptcha_v3[:site_key]
+# frozen_string_literal: true
+
+if ENV['ASSET_PRECOMPILE'].blank?
+  Recaptcha.configure do |config|
+    config.site_key = Rails.application.credentials.recaptcha_v3[:site_key]
     config.secret_key = Rails.application.credentials.recaptcha_v3[:secret_key]
-  
+
     # Uncomment the following line if you are using a proxy server:
     # config.proxy = 'http://myproxy.com.au:8080'
-  
+
     # Uncomment the following lines if you are using the Enterprise API:
     # config.enterprise = true
     # config.enterprise_api_key = <key>
     # config.enterprise_project_id = 'my-project'
   end
+end
